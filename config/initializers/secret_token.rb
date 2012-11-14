edb206fdaadb22abb5afb8999c377575f2096444
Ana Pedrosa <anacpedrosa@gmail.com>
2012-11-14 21:59:28 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loja::Application.config.secret_token = '80bf6a275c8adf1e77295a71badd03fa7e99fdcafd3756dfabc8d58e589eed322e2360cc3cf603ca56aedee9b01568b7d01a4eaf80b8f00c74219bf9f5ee568c'
